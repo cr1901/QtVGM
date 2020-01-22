d6f4b34987aa2ea408d7bdb136cccefd86b23b7e
William D. Jones <thor0505@comcast.net>
2020-01-21 23:46:37 -0500

    Dead code cleanup.

--- playthread.cpp
+++ playthread.cpp
@@ -11,15 +11,10 @@
 #include <string.h>
 #include <ctype.h>
 #include <vector>
-#include <string>
 
-#ifdef _WIN32
-extern "C" int __cdecl _getch(void);	// from conio.h
-extern "C" int __cdecl _kbhit(void);
-#else
+#ifndef _WIN32
 #include <unistd.h>		// for STDIN_FILENO and usleep()
 #include <termios.h>
-#include <sys/time.h>	// for struct timeval in _kbhit()
 #define	Sleep(msec)	usleep(msec * 1000)
 #endif
 
@@ -36,12 +31,8 @@ extern "C" int __cdecl _kbhit(void);
 #include <utils/FileLoader.h>
 #include <utils/OSMutex.h>
 
-//#define USE_MEMORY_LOADER 1	// define to use the in-memory loader
-static std::string FCC2Str(UINT32 fcc);
-static const char* GetFileTitle(const char* filePath);
 static UINT32 CalcCurrentVolume(UINT32 playbackSmpl);
 static UINT32 FillBuffer(void* drvStruct, void* userParam, UINT32 bufSize, void* Data);
-static UINT8 FilePlayCallback(PlayerBase* player, void* userParam, UINT8 evtType, void* evtParam);
 static UINT32 GetNthAudioDriver(UINT8 adrvType, INT32 drvNumber);
 static UINT8 InitAudioSystem(void);
 static UINT8 DeinitAudioSystem(void);
@@ -53,8 +44,6 @@ char* GetLastDirSeparator(const char* FilePath);
 static bool IsAbsolutePath(const char* FilePath);
 #ifndef _WIN32
 static void changemode(UINT8 noEcho);
-static int _kbhit(void);
-#define	_getch	getchar
 #endif
 
 static UINT32 smplSize;
@@ -81,9 +70,6 @@ static INT32 WaveWrtDrv = -1;
 static UINT32 masterVol = 0x10000;	// fixed point 16.16
 static UINT32 fadeSmplStart;
 static UINT32 fadeSmplTime;
-
-static bool showTags = true;
-static bool showFileInfo = true;
 
 static VGMPlayer* vgmPlr;
 
@@ -137,13 +123,11 @@ void PlayThread::run() {
 
 	retVal = InitAudioSystem();
 	if (retVal)
-		//return 1;
     return;
 	retVal = StartAudioDevice();
 	if (retVal)
 	{
 		DeinitAudioSystem();
-		//return 1;
     return;
 	}
 	playState = 0x00;
@@ -155,7 +139,6 @@ void PlayThread::run() {
 
   if(!OpenPlayListFile(m3uFile, &PlayListFileContents))
   {
-    fprintf(stderr, "Error opening the playlist!\n");
     return;
   }
 
@@ -163,10 +146,6 @@ void PlayThread::run() {
   {
     char vgmFile[MAX_PATH];
 
-  	// for (curSong = argbase; curSong < argc; curSong ++)
-  	// {
-
-  	printf("Loading %s ...  ", GetFileTitle(PlayListFileContents[CurPLFile]));
   	fflush(stdout);
   	player = NULL;
 
@@ -181,8 +160,6 @@ void PlayThread::run() {
     }
 
   	dLoad = FileLoader_Init(vgmFile);
-    //printf("%X, %X", curSong, dLoad->_status);
-
   	if(dLoad == NULL) continue;
 
   	DataLoader_SetPreloadBytes(dLoad,0x100);
@@ -190,7 +167,6 @@ void PlayThread::run() {
   	if (retVal)
   	{
   		DataLoader_CancelLoading(dLoad);
-  		fprintf(stderr, "Error 0x%02X loading file!\n", retVal);
   		continue;
   	}
 
@@ -201,15 +177,11 @@ void PlayThread::run() {
   	{
   		DataLoader_CancelLoading(dLoad);
   		player = NULL;
-  		fprintf(stderr, "Error 0x%02X loading file!\n", retVal);
   		continue;
   	}
 
   	VGMPlayer* vgmplay = dynamic_cast<VGMPlayer*>(player);
   	const VGM_HEADER* vgmhdr = vgmplay->GetFileHeader();
-
-  	printf("VGM v%3X, Total Length: %.2f s, Loop Length: %.2f s", vgmhdr->fileVer,
-  			player->Tick2Second(player->GetTotalTicks()), player->Tick2Second(player->GetLoopTicks()));
 
   	const char* songTitle = NULL;
   	const char* songAuthor = NULL;
@@ -253,8 +225,6 @@ void PlayThread::run() {
 
     emit newSong(infoBuf);
 
-  	putchar('\n');
-
   	player->SetSampleRate(sampleRate);
   	player->Start();
   	fadeSmplTime = player->GetSampleRate() * 4;
@@ -262,26 +232,26 @@ void PlayThread::run() {
     // TODO: Make this not hardcoded.
   	fadeSmplStart = player->Tick2Sample(player->GetTotalPlayTicks(maxLoops));
 
-  	if (showFileInfo)
-  	{
-  		PLR_SONG_INFO sInf;
-  		std::vector<PLR_DEV_INFO> diList;
-  		size_t curDev;
-
-  		player->GetSongInfo(sInf);
-  		player->GetSongDeviceInfo(diList);
-  		printf("SongInfo: %s v%X.%X, Rate %u/%u, Len %u, Loop at %d, devices: %u\n",
-  			FCC2Str(sInf.format).c_str(), sInf.fileVerMaj, sInf.fileVerMin,
-  			sInf.tickRateMul, sInf.tickRateDiv, sInf.songLen, sInf.loopTick, sInf.deviceCnt);
-  		for (curDev = 0; curDev < diList.size(); curDev ++)
-  		{
-  			const PLR_DEV_INFO& pdi = diList[curDev];
-  			printf(" Dev %d: Type 0x%02X #%d, Core %s, Clock %u, Rate %u, Volume 0x%X\n",
-  				(int)pdi.id, pdi.type, (INT8)pdi.instance, FCC2Str(pdi.core).c_str(), pdi.clock, pdi.smplRate, pdi.volume);
-  			if (pdi.cParams != 0)
-  				printf("        CfgParams: 0x%08X\n", pdi.cParams);
-  		}
-  	}
+  	// if (showFileInfo)
+  	// {
+  	// 	PLR_SONG_INFO sInf;
+  	// 	std::vector<PLR_DEV_INFO> diList;
+  	// 	size_t curDev;
+    //
+  	// 	player->GetSongInfo(sInf);
+  	// 	player->GetSongDeviceInfo(diList);
+  	// 	printf("SongInfo: %s v%X.%X, Rate %u/%u, Len %u, Loop at %d, devices: %u\n",
+  	// 		FCC2Str(sInf.format).c_str(), sInf.fileVerMaj, sInf.fileVerMin,
+  	// 		sInf.tickRateMul, sInf.tickRateDiv, sInf.songLen, sInf.loopTick, sInf.deviceCnt);
+  	// 	for (curDev = 0; curDev < diList.size(); curDev ++)
+  	// 	{
+  	// 		const PLR_DEV_INFO& pdi = diList[curDev];
+  	// 		printf(" Dev %d: Type 0x%02X #%d, Core %s, Clock %u, Rate %u, Volume 0x%X\n",
+  	// 			(int)pdi.id, pdi.type, (INT8)pdi.instance, FCC2Str(pdi.core).c_str(), pdi.clock, pdi.smplRate, pdi.volume);
+  	// 		if (pdi.cParams != 0)
+  	// 			printf("        CfgParams: 0x%08X\n", pdi.cParams);
+  	// 	}
+  	// }
 
   	if (audDrv != NULL)
   		retVal = AudioDrv_SetCallback(audDrv, FillBuffer, &player);
@@ -295,22 +265,6 @@ void PlayThread::run() {
   	needRefresh = true;
   	while(! (playState & PLAYSTATE_END))
   	{
-  		if (! (playState & PLAYSTATE_PAUSE))
-  			needRefresh = true;	// always update when playing
-  		if (needRefresh)
-  		{
-  			const char* pState;
-
-  			if (playState & PLAYSTATE_PAUSE)
-  				pState = "Paused";
-  			else
-  				pState = "Playing";
-  			printf("%s %.2f / %.2f ...   \r", pState, player->Sample2Second(player->GetCurPos(PLAYPOS_SAMPLE)),
-  					player->Tick2Second(player->GetTotalPlayTicks(maxLoops)));
-  			fflush(stdout);
-  			needRefresh = false;
-  		}
-
   		if (manualRenderLoop && ! (playState & PLAYSTATE_PAUSE))
   		{
   			UINT32 wrtBytes = FillBuffer(audDrvLog, &player, localAudBufSize, localAudBuffer);
@@ -374,7 +328,6 @@ void PlayThread::run() {
 
 	StopAudioDevice();
 	DeinitAudioSystem();
-	printf("Done.\n");
 
 #if defined(_DEBUG) && (_MSC_VER >= 1400)
 	// doesn't work well with C++ containers
@@ -382,31 +335,7 @@ void PlayThread::run() {
 	//	_getch();
 #endif
 
-	//return 0;
   return;
-}
-
-static std::string FCC2Str(UINT32 fcc)
-{
-	std::string result(4, '\0');
-	result[0] = (char)((fcc >> 24) & 0xFF);
-	result[1] = (char)((fcc >> 16) & 0xFF);
-	result[2] = (char)((fcc >>  8) & 0xFF);
-	result[3] = (char)((fcc >>  0) & 0xFF);
-	return result;
-}
-
-static const char* GetFileTitle(const char* filePath)
-{
-	const char* dirSep1;
-	const char* dirSep2;
-
-	dirSep1 = strrchr(filePath, '/');
-	dirSep2 = strrchr(filePath, '\\');
-	if (dirSep2 > dirSep1)
-		dirSep1 = dirSep2;
-
-	return (dirSep1 == NULL) ? filePath : (dirSep1 + 1);
 }
 
 #if 1
@@ -473,7 +402,6 @@ static UINT32 FillBuffer(void* drvStruct, void* userParam, UINT32 bufSize, void*
 	}
 	if (! (player->GetState() & PLAYSTATE_PLAY))
 	{
-		fprintf(stderr, "Player Warning: calling Render while not playing! playState = 0x%02X\n", player->GetState());
 		memset(data, 0x00, smplCount * smplSize);
 		return smplCount * smplSize;
 	}
@@ -567,7 +495,6 @@ static UINT8 InitAudioSystem(void)
 
 	retVal = OSMutex_Init(&renderMtx, 0);
 
-	printf("Opening Audio Device ...\n");
 	retVal = Audio_Init();
 	if (retVal == AERR_NODRVS)
 		return retVal;
@@ -576,7 +503,6 @@ static UINT8 InitAudioSystem(void)
 	idWavOutDev = 0;	// default device
 	if (AudioOutDrv != -1 && idWavOut == (UINT32)-1)
 	{
-		fprintf(stderr, "Requested Audio Output driver not found!\n");
 		Audio_Deinit();
 		return AERR_NODRVS;
 	}
@@ -586,11 +512,9 @@ static UINT8 InitAudioSystem(void)
 	if (idWavOut != (UINT32)-1)
 	{
 		Audio_GetDriverInfo(idWavOut, &drvInfo);
-		printf("Using driver %s.\n", drvInfo->drvName);
 		retVal = AudioDrv_Init(idWavOut, &audDrv);
 		if (retVal)
 		{
-			fprintf(stderr, "WaveOut: Driver Init Error: %02X\n", retVal);
 			Audio_Deinit();
 			return retVal;
 		}
@@ -653,11 +577,9 @@ static UINT8 StartAudioDevice(void)
 
 	if (audDrv != NULL)
 	{
-		printf("Opening Device %u ...\n", idWavOutDev);
 		retVal = AudioDrv_Start(audDrv, idWavOutDev);
 		if (retVal)
 		{
-			fprintf(stderr, "Device Init Error: %02X\n", retVal);
 			return retVal;
 		}
 
@@ -719,23 +641,7 @@ static void changemode(UINT8 noEcho)
 
 	return;
 }
-
-static int _kbhit(void)
-{
-	struct timeval tv;
-	fd_set rdfs;
-
-	tv.tv_sec = 0;
-	tv.tv_usec = 0;
-
-	FD_ZERO(&rdfs);
-	FD_SET(STDIN_FILENO, &rdfs);
-	select(STDIN_FILENO + 1, &rdfs, NULL, NULL, &tv);
-
-	return FD_ISSET(STDIN_FILENO, &rdfs);;
-}
 #endif
-
 
 static bool OpenPlayListFile(const char* FileName, char*** PlayListFile)
 {
@@ -851,8 +757,6 @@ static bool OpenPlayListFile(const char* FileName, char*** PlayListFile)
 	{
 		strcpy(PLFileBase, "");
 	}
-
-  printf("%s", PLFileBase);
 
 	return true;
 }
